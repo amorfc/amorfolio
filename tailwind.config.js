74b6c2a0dc73d96b6c1dea06ad1959ba591365f8
amorf <fatihermetin@gmail.com>
2024-01-12 15:17:36 -0500

    Add temp tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#525252",
+      secondary: "#414141",
+      ternary: "#313131",
+      danger: "#CA3E47",
+    },
+    boxShadow: {
+      "neu-primary": ["20px 20px 60px #414141", "-20px -20px 60px #525252"],
+    },
   },
   plugins: [],
 };
