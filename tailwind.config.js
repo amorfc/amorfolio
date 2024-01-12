9ea22bd179a77ab5f20c2913ce9b9cc1b761bae7
amorf <fatihermetin@gmail.com>
2024-01-12 18:00:30 -0500

    Extend taiwlind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#525252",
-      secondary: "#414141",
-      ternary: "#313131",
-      danger: "#CA3E47",
-    },
-    boxShadow: {
-      "neu-primary": ["20px 20px 60px #414141", "-20px -20px 60px #525252"],
+    extend: {
+      colors: {
+        primary: "#525252",
+        secondary: "#414141",
+        ternary: "#313131",
+        danger: "#CA3E47",
+      },
+      boxShadow: {
+        "neu-primary": ["20px 20px 60px #414141", "-20px -20px 60px #525252"],
+      },
     },
   },
   plugins: [],
