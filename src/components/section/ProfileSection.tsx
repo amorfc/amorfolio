c2ecd161df7504663b327addb165ea35e057d339
amorf <fatihermetin@gmail.com>
2024-01-14 20:27:30 -0500

    Add profile section data

--- src/components/section/ProfileSection.tsx
+++ src/components/section/ProfileSection.tsx
@@ -1,16 +1,29 @@
-// interface ProfileSectionProps{}
-
+import { profileSection } from "../../data/profileSection";
+import AppImage from "../image/AppImage";
+import BaseText from "../text/BaseText";
+import BaseCircleView from "../view/BaseCircleView";
 import BaseView from "../view/BaseView";
 import { HomeSection } from "./HomeSection";
 
 export const ProfileSection = () => {
+  const baseCircleViewClassName =
+    "neu-circle m-8 w-[25vh] h-[25vh] lg:w-[35vh] lg:h-[35vh]";
   return (
     <HomeSection>
-      <BaseView className="basis-[35%] bg-danger items-center justify-center">
-        <div className="bg-white m-8 rounded-full w-[25vh] h-[25vh] lg:w-[35vh] lg:h-[35vh]"></div>
+      <BaseView className="basis-[35%] items-center justify-center">
+        <BaseCircleView className={baseCircleViewClassName}>
+          <AppImage
+            className={"p-4 rounded-full object-cover w-[100%] h-[100%]"}
+            imagePublicUrl={"profile.png"}
+            alt="Profile Photo"
+          />
+        </BaseCircleView>
       </BaseView>
-      <BaseView className="basis-[65%] bg-danger">
-        <h1></h1>
+      <BaseView className="neu-rectangle inset rounded-3xl basis-[65%] m-5 p-4 md:p-8 text-center items-center justify-center">
+        <BaseText
+          className="sm:text-lg md:text-xl text-center"
+          text={profileSection.profile.description}
+        />
       </BaseView>
     </HomeSection>
   );
---
+++ src/data/profileSection.ts
@@ -0,0 +1,7 @@
+export const profileSection = {
+  profile: {
+    title: "Profile",
+    description:
+      "Bringing over 3 years of expertise, adept atdelivering flawless code under tightdeadlines. Proficient in Agile frameworks,excelling both collaboratively andindependently. Specialized in application,form, and database development usingJavaScript, TypeScript, React, NodeJS,Kotlin, Android SDK, React Native, Expo,Flutter, Rust, and MongoDB. Team-orientedand diligent, offering strong communicationskills. Recognized for providing high-qualitycode, subject matter expertise, andcollaboration. Skilled in data analytics andreport writing.",
+  },
+};
