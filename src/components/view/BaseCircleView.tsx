b119c62df10f4b3a2e51d5b40518ee6a70ec412e
amorf <fatihermetin@gmail.com>
2024-01-16 17:12:01 -0500

    Add base circle view size prop

--- src/components/view/BaseCircleView.tsx
+++ src/components/view/BaseCircleView.tsx
@@ -1,22 +1,22 @@
 import { PropsWithChildren, useMemo } from "react";
 import { PropsWithTwClassName } from "../../common/propsInterfaces";
 import { styleMerge } from "../../utils/styleMerge";
-import { BaseCircleViewSize } from "../../constants/imageSizeStyleConstants";
+import { SizeContants } from "../../constants/sizeStyleConstants";
 
 interface BaseCircleViewProps extends PropsWithTwClassName, PropsWithChildren {
-  size?: BaseCircleViewSize;
+  size?: SizeContants;
 }
 
 const BaseCircleView = (props: BaseCircleViewProps) => {
-  const { className, children, size = BaseCircleViewSize.large } = props;
+  const { className, children, size = SizeContants.large } = props;
 
   const sizeStyle = useMemo(() => {
     switch (size) {
-      case BaseCircleViewSize.medium:
+      case SizeContants.medium:
         return "w-[16vh] h-[16vh] lg:w-[20vh] lg:h-[20vh]";
-      case BaseCircleViewSize.large:
+      case SizeContants.large:
         return "w-[25vh] h-[25vh] lg:w-[35vh] lg:h-[35vh]";
-      case BaseCircleViewSize.small:
+      case SizeContants.small:
         return "w-[5vh] h-[5vh] lg:w-[7vh] lg:h-[7vh]";
     }
   }, [size]);
--- src/constants/sizeStyleConstants.ts
+++ src/constants/sizeStyleConstants.ts
@@ -1,4 +1,4 @@
-export enum BaseCircleViewSize {
+export enum SizeContants {
   medium = "medium",
   large = "large",
   small = "small",
