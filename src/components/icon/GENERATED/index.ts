6ae2eeb5048588311d04298cbe9422bdc036cd72
amorf <fatihermetin@gmail.com>
2024-01-17 18:54:33 -0500

    Export generated components

---
+++ src/components/icon/GENERATED/index.ts
@@ -0,0 +1,4 @@
+export { default as GithubLogo } from "./GithubLogo";
+export { default as LinkedinLogo } from "./LinkedinLogo";
+export { default as LinkedinLogo1 } from "./LinkedinLogo1";
+export { default as TelegramLogo } from "./TelegramLogo";
