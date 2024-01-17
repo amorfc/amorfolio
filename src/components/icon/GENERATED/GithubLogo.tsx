67f51927387d384ac5bf548971df78bf501be767
amorf <fatihermetin@gmail.com>
2024-01-17 18:54:47 -0500

    Generated files

---
+++ src/components/icon/GENERATED/GithubLogo.tsx
@@ -0,0 +1,17 @@
+// Auto-generated file created by svgr-cli source custom svg-template.ts
+// Run pnpm icons:generate to update
+// Do not edit
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgGithubLogo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={props.width || 256}
+    height={props.height || 256}
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M19 0H5a5 5 0 0 0-5 5v14a5 5 0 0 0 5 5h14a5 5 0 0 0 5-5V5a5 5 0 0 0-5-5m-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82A7.6 7.6 0 0 0 12 7.868a7.6 7.6 0 0 0-2.003.269c-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118a3.1 3.1 0 0 0-.824 2.147c0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385A8 8 0 0 1 12 4a8 8 0 0 1 2.534 15.59" />
+  </svg>
+);
+export default SvgGithubLogo;
