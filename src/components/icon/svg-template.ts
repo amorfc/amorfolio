c83f818d733884677693151f4b7f7c2072fdc3b3
amorf <fatihermetin@gmail.com>
2024-01-17 18:54:24 -0500

    Add svgr component generation template

---
+++ src/components/icon/svg-template.ts
@@ -0,0 +1,18 @@
+const comments = `
+// Auto-generated file created by svgr-cli source custom svg-template.ts
+// Run pnpm icons:generate to update
+// Do not edit
+`;
+const template = (variables, { tpl }) => {
+  return tpl`
+${comments}
+${variables.imports};
+${variables.interfaces};
+const ${variables.componentName} = (${variables.props}) => (
+  ${variables.jsx}
+);
+${variables.exports};
+`;
+};
+
+module.exports = template;
