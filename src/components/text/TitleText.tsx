59cba31f35c317cf510383309ac7b42a2ffa44e9
amorf <fatihermetin@gmail.com>
2024-01-20 16:19:19 -0500

    Fix xsm size intitle text

--- src/components/text/TitleText.tsx
+++ src/components/text/TitleText.tsx
@@ -12,7 +12,7 @@ const TitleText = (props: TitleTextProps) => {
   const sizeStyle = useMemo(() => {
     switch (size) {
       case SizeContants.xxsmall:
-        return "sm:text-xsm md:text-sm lg:text-md";
+        return "text-sm lg:text-md";
       case SizeContants.xsmall:
         return "sm:text-sm md:text-md lg:text-lg";
       case SizeContants.small:
