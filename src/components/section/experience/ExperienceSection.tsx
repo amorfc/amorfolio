8efbb9642ee142456c5ede20d2d5a05f70ebca9a
amorf <fatihermetin@gmail.com>
2024-01-20 20:36:57 -0500

    Add use screen detector

--- src/components/section/experience/ExperienceSection.tsx
+++ src/components/section/experience/ExperienceSection.tsx
@@ -1,4 +1,6 @@
 import { ExperienceType } from "../../../data/experience/experienceData";
+import { useScreenDetector } from "../../../hooks/useScreenDetector";
+import { styleMerge } from "../../../utils/styleMerge";
 import BaseView from "../../view/BaseView";
 import ExperienceSectionDescriptionView from "./ExperienceSectionDescriptionView";
 import ExperienceSectionLogo from "./ExperienceSectionLogo";
@@ -21,16 +23,18 @@ const ExperienceSection = (props: ExperienceSectionProps) => {
     },
   } = props;
 
+  const { isMobile } = useScreenDetector();
+
   return (
-    <BaseView className="w-full h-full overflow-hidden justify-between">
-      <BaseView className="md:flex-row">
-        <BaseView className="basis-[20%] items-center justify-center">
+    <BaseView className="flex-none md:flex-1 w-full h-full overflow-hidden justify-between">
+      <BaseView className="flex-none md:flex-row">
+        <BaseView className="flex-none md:basis-[20%] items-center justify-center">
           <ExperienceSectionLogo
             imgPublicUrl={imgPublicUrl}
             company={company}
           />
         </BaseView>
-        <BaseView className="basis-[80%] p-2">
+        <BaseView className="flex-none md:basis-[80%] p-2">
           <ExperienceSectionDescriptionView
             company={company}
             shortDescription={shortDescription}
@@ -40,7 +44,12 @@ const ExperienceSection = (props: ExperienceSectionProps) => {
           />
         </BaseView>
       </BaseView>
-      <BaseView className="flex-none flex-0 my-1 px-1 py-2 items-start justify-start">
+      <BaseView
+        className={styleMerge(
+          isMobile && "hidden",
+          "flex-none flex-0 my-1 px-1 py-2 items-start justify-start"
+        )}
+      >
         <ExperienceSectionSkillList
           className={"rounded-xl items-center justify-center gap-2"}
           data={skills}
