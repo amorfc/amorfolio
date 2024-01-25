9657498be023e496aae777128934e93d91964508
amorf <fatihermetin@gmail.com>
2024-01-25 17:01:13 -0500

    Add animation style tailwind config hook

---
+++ src/hooks/style/useTwAnimation.ts
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+// Add your animation here if you like to expand the animation options from tailwind animated
+type TwAppAnimationType = "fade-up" | "fade-down";
+
+interface UseTwAnimationParams {
+  anim: TwAppAnimationType;
+  durationMs?: number;
+}
+
+export const useTwAppAnimation = (params: UseTwAnimationParams): string => {
+  const { anim, durationMs = 600 } = params;
+
+  const result = useMemo(() => {
+    const durationRes = durationMs;
+    const twAnimation = getTwAnimation(anim);
+
+    return `${twAnimation} animate-once animate-duration-[${durationRes}ms] animate-ease-in-out`;
+  }, [anim, durationMs]);
+  return result;
+};
+
+const getTwAnimation = (anim: TwAppAnimationType) => {
+  return `animate-${anim}`;
+};
