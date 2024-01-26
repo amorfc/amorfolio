2102bc38840630bf2468cf0f599dd1aacce86535
amorf <fatihermetin@gmail.com>
2024-01-25 19:02:26 -0500

    Add use scroll into view hook

---
+++ src/hooks/scroll/useAppScrollIntoView.ts
@@ -0,0 +1,28 @@
+import { useCallback, RefObject } from "react";
+
+interface ScrollIntoViewOptions {
+  behavior?: ScrollBehavior;
+  block?: ScrollLogicalPosition;
+  inline?: ScrollLogicalPosition;
+}
+
+const useScrollIntoView = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>
+) => {
+  const scrollIntoView = useCallback(
+    (
+      options: ScrollIntoViewOptions = {
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+      }
+    ) => {
+      ref.current?.scrollIntoView(options);
+    },
+    [ref]
+  );
+
+  return scrollIntoView;
+};
+
+export default useScrollIntoView;
