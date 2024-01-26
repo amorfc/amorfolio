52561642f236b754bc2aa0256988a895348644c9
amorf <fatihermetin@gmail.com>
2024-01-26 14:41:23 -0500

    Fix experience section logo styling

--- src/components/section/experience/ExperienceSectionLogo.tsx
+++ src/components/section/experience/ExperienceSectionLogo.tsx
@@ -10,7 +10,7 @@ const ExperienceSectionLogo = (props: ExperienceSectionLogoProps) => {
   const { imgPublicUrl, company } = props;
   return (
     <NeuCircleImage
-      className="p-4 rounded-full object-cover w-[100%] h-[100%] opacity-70"
+      imageClassName="p-4 rounded-full object-cover w-[100%] h-[100%] opacity-70"
       imgPublicUrl={imgPublicUrl}
       alt={`${company}_logo_alt`}
     />
