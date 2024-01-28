44d911d96b6385d82450892c047a4c1a8680825a
amorf <fatihermetin@gmail.com>
2024-01-27 20:46:55 -0500

    Add py safe for iphones

--- src/components/view/AppLayout.tsx
+++ src/components/view/AppLayout.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from "react";
 import { PropsWithTwClassName } from "../../common/propsInterfaces";
 import { styleMerge } from "../../utils/styleMerge";
+import { useScreenDetector } from "../../hooks/useScreenDetector";
 
 interface AppLayoutProps extends PropsWithChildren, PropsWithTwClassName {}
 
 const AppLayout = (props: AppLayoutProps) => {
   const { className } = props;
+  const { isMobile, isTablet } = useScreenDetector();
   const baseClassName = styleMerge(
-    "flex flex-1 bg-main lg:h-[100vh] p-6",
+    "flex flex-1 bg-main lg:h-[100vh] p-5",
+    isMobile || isTablet ? "py-safe" : "",
     className
   );
   return <div className={baseClassName}>{props.children}</div>;
