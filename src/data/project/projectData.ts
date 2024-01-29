a9c674e53ccf9757c7dd5420b1a9a9a29b110c57
amorf <fatihermetin@gmail.com>
2024-01-29 16:17:53 -0500

    Add project data and fix some props

--- src/data/project/projectData.ts
+++ src/data/project/projectData.ts
@@ -10,13 +10,11 @@ export interface ProjectType {
 
 export const projectData: ProjectType[] = [
   {
-    projectName: "Nym Updater",
-    description: `
-    I developed a terminal application designed for the automatic updating of Nym mixnodes and gateways, contributing to the efficiency and reliability of the Nym privacy-centric cryptocurrency network. The application streamlines the update process, ensuring that Nym nodes stay current with the latest features, security patches, and improvements.
-    `,
-    imgPublicUrl: ImagePublicUrls.Nym,
-    redirectUrl: "https://nymtech.net/",
-    sourceCodeUrl: "https://github.com/amorfc/nym-updater",
+    projectName: "Amorfolio",
+    description: `You are here! Amorfolio is a portfolio website template designed to showcase your work and experience. It is built with React, TypeScript, and TailwindCSS. The website is fully responsive and customizable. The source code is available for free on GitHub.`,
+    imgPublicUrl: ImagePublicUrls.Profile,
+    redirectUrl: "https://amorfolio/",
+    sourceCodeUrl: "https://github.com/orgs/hoodrunio/dashboard",
   },
   {
     projectName: "Hoodrun",
@@ -26,10 +24,20 @@ export const projectData: ProjectType[] = [
     sourceCodeUrl: "https://github.com/orgs/hoodrunio/dashboard",
   },
   {
-    projectName: "Amorfolio",
-    description: `You are here! Amorfolio is a portfolio website template designed to showcase your work and experience. It is built with React, TypeScript, and TailwindCSS. The website is fully responsive and customizable. The source code is available for free on GitHub.`,
-    imgPublicUrl: ImagePublicUrls.Hoodrun,
-    redirectUrl: "https://amorfolio/",
-    sourceCodeUrl: "https://github.com/orgs/hoodrunio/dashboard",
+    projectName: "Nym Updater",
+    description: `
+    I developed a terminal application designed for the automatic updating of Nym mixnodes and gateways, contributing to the efficiency and reliability of the Nym privacy-centric cryptocurrency network. The application streamlines the update process, ensuring that Nym nodes stay current with the latest features, security patches, and improvements.
+    `,
+    imgPublicUrl: ImagePublicUrls.Nym,
+    redirectUrl: "https://nymtech.net/",
+    sourceCodeUrl: "https://github.com/amorfc/nym-updater",
+  },
+  {
+    projectName: "Blockchain Explorer",
+    description: `
+    Developed a blockchain explorer for Cosmos chain users. Which shows all information about the blocks, transactions, accounts, tokens and many others. The backend is developed in Rust and the frontend is developed in React. The backend is open source and available on GitHub. The frontend is available on request.
+    `,
+    imgPublicUrl: ImagePublicUrls.BlockchainExplorer,
+    sourceCodeUrl: "https://github.com/hoodrunio/explorer-backend",
   },
 ];
