7e21c48ae2fda281ad1e97f18edd8e8a834ba3ff
amorf <fatihermetin@gmail.com>
2024-02-05 17:03:44 -0500

    Change intro text

--- src/components/introduction/Introduction.tsx
+++ src/components/introduction/Introduction.tsx
@@ -65,4 +65,4 @@ const Introduction = (props: IntroductionProps) => {
 
 export default Introduction;
 
-const text = ` Hello! Feel free to scroll :)  I'm Fatih Ermetin, also known as Amorf. I'm not just a software developer; I'm also a musician and vocalist. While composing songs, my passion lies in developing mobile applications and blockchain tools, contributing to open-source projects.I'm well-versed in the Cosmos ecosystem. Although I began my career in Fintech, I've explored other fields. Throughout my journey, I've consistently adhered to best practices and technical design patterns.`;
+const text = `Hello! Feel free to scroll :) I'm Fatih Ermetin, also known as Amorf. I bring a unique blend of expertise as both a software developer and a musician/vocalist. While composing songs, my passion lies in Front End development, crafting mobile applications, and contributing to open-source projects. Despite starting my career in Fintech, I've diversified my skills across various fields, consistently applying best practices and technical design patterns.`;
